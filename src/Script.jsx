ffc48a464b84e03d7874cc822384f4e6c5607380
Brian Simiyu <briansimiyuj@gmail.com>
2023-10-11 14:23:13 +0300

    Fetching data

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from 'react';
+
 function Script() {
+
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+
+  const [reqType, setReqType] = useState('users'),
+
+        [items, setItems] = useState([])
+
+  useEffect(() =>{
+  
+    const fetchItems = async () =>{
+
+      try{
+
+        const response = await fetch(`${API_URL}${reqType}`),
+
+              data = await response.json()
+
+        setItems(data)
+
+      }catch(err){
+
+        console.log(err)
+
+      }
+
+    }
+
+    fetchItems()
+  
+  }, [reqType])
+
+
+
   return (
     <div className="script">
-
+      
     </div>
   );
 }
